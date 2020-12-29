6ab28406ad651da0f0eb23aacd1117916c6d6d72
Vlad <vladbukatin46@gmail.com>
2020-12-29 15:09:36 +0200

    CMS lab

---
+++ GameStore.WebUI/Models/GameViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GameStore.Domain.Entities;
+
+namespace GameStore.WebUI.Models
+{
+    public class GameViewModel
+    {
+        public Game Game1 { get; set; }
+    }
+}
---
+++ GameStore.WebUI/Models/PagingInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GameStore.WebUI.Models
+{
+    public class PagingInfo
+    {
+        public int TotalItems { get; set; }
+
+        public String ParentPage { get; set; }
+
+        public int ItemsPerPage { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        public DateTime Created_at { get; set; }
+
+        public int TotalPages
+        {
+            get { return (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage); }
+        }
+    }
+}
